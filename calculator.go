49def20896b0a4867d2764cc99c66e1f203ff661
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-05-10 22:51:35 +0700

    Create findArea

--- calculator.go
+++ calculator.go
@@ -5,10 +5,14 @@ import (
   "math"
 )
 
+func findArea(number_a, number_b float64) float64 {
+  return number_a * number_b * math.Pi
+}
+
 func main() {
   var number_a float64
   var number_b float64
 
   fmt.Scanf("%d %d", &number_a, &number_b)
-  fmt.Println(number_a * number_b * math.Pi)
+  fmt.Println(findArea(number_a, number_b))
 }
